e75b63071adb9b5c12e795aee2a6ac9b7d691e6a
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-04-12 17:05:16 +0200

    responsive cards

--- src/components/body/curriculum/Cv.jsx
+++ src/components/body/curriculum/Cv.jsx
@@ -28,7 +28,7 @@ const PdfViewer = () => {
     <div className="containerPdf" data-aos="zoom-in">
       <div className="blockPdf">
         <Document file={"pdf/CVFriedrichThierry.pdf"}>
-          <Page pageNumber={1} width={260} />
+          <Page pageNumber={1} width={240} />
         </Document>
       </div>
       <div
