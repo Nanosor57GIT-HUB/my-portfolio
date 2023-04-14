7210a822d813349e168d52381a3ede8e53a383fa
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-04-14 17:27:34 +0200

    ...

--- src/components/footer/LogoDeveloppeur.jsx
+++ src/components/footer/LogoDeveloppeur.jsx
@@ -4,37 +4,37 @@ const LogoDeveloppeur = () => {
   return (
     <div className="containerLogoDev">
       <img
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoHTML5.png"}
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoHTML5.webp"}
         className="logoDev"
         alt="logo_Html5"
       />
       <img 
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoCSS3.png"} 
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoCSS3.webp"} 
         className="logoDev" 
         alt="logo_Css3" 
         />
       <img 
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoSass.png"} 
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoSass.webp"} 
         className="logoDev" 
         alt="logo_Sass" 
         />
       <img
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoJavascript.png"}
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoJavascript.webp"}
         className="logoDev"
         alt="logo_Javascript"
       />
       <img
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoReact.png"}
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoReact.webp"}
         className="logoDev"
         alt="logo_React"
       />
       <img
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNodejs.png"}
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNodejs.webp"}
         className="logoDev"
         alt="logo_NodeJs"
       />
       <img 
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNPM.png"} 
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNPM.webp"} 
         className="logoDev" 
         alt="logo_Npm" 
         />
