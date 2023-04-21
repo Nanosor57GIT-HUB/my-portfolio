7ee778dc5346fb9397f4e8364f3b94067cedddf6
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-04-21 10:20:56 +0200

    test5 offset

--- src/components/body/projets/FormationCards.jsx
+++ src/components/body/projets/FormationCards.jsx
@@ -45,7 +45,7 @@ const FormationCards = ({pro}) => {
         {pro.map((item) => (
           <div className="cards" key={item.id}>
             <p className="description">{item.description}</p>
-            <div className="card" data-aos="fade" data-aos-anchor-placement="top-bottom" data-aos-offset="100">            
+            <div className="card" data-aos="fade" data-aos-anchor-placement="top-bottom">            
               <div className="containerCover" >
               
                 <img
