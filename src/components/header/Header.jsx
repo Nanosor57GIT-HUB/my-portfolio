89610e24ab633ec1855d834d4e1ace7ffd5cdc3d
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-04-25 21:10:09 +0200

    test2 aria

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -33,6 +33,7 @@ const Header = () => {
           rel="noreferrer"
           target={"_blank"}
           className=" linkedIn"
+          aria-label="Voir mon profil sur LinkedIn"
         >
           <img
             src="images/LinkedIn-icon.webp"
