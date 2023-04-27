acca9418e8b58b3ce51150405da5caeb43801b67
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-04-27 21:00:26 +0200

    tesst x

--- src/components/footer/LogoDeveloppeur.jsx
+++ src/components/footer/LogoDeveloppeur.jsx
@@ -7,43 +7,43 @@ const LogoDeveloppeur = () => {
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoHTML5(20x20).png"}
         className="logoDev"
         alt="logo_Html5"
-        style={{ maxWidth: 40, maxHeight: 40,}}
+        style={{ maxWidth: 50, maxHeight: 40,}}
       />
       <img 
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoCSS3(20x20).png"} 
         className="logoDev" 
         alt="logo_Css3" 
-        style={{ maxWidth: 40, maxHeight: 40,}}
+        style={{ maxWidth: 50, maxHeight: 40,}}
         />
       <img 
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoSass(25x20).png"} 
         className="logoDev" 
         alt="logo_Sass" 
-        style={{ maxWidth: 40, maxHeight: 40,}}
+        style={{ maxWidth: 50, maxHeight: 40,}}
         />
       <img
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoJavascript(25x20).png"}
         className="logoDev"
         alt="logo_Javascript"
-        style={{ maxWidth: 40, maxHeight: 40,}}
+        style={{ maxWidth: 50, maxHeight: 40,}}
       />
       <img
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoReact(25x20).png"}
         className="logoDev"
         alt="logo_React"
-        style={{ maxWidth: 40, maxHeight: 40,}}
+        style={{ maxWidth: 50, maxHeight: 40,}}
       />
       <img
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNodejs(25x20).png"}
         className="logoDev"
         alt="logo_NodeJs"
-        style={{ maxWidth: 40, maxHeight: 40,}}
+        style={{ maxWidth: 50, maxHeight: 40,}}
       />
       <img 
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNPM(25x20).png"} 
         className="logoDev" 
         alt="logo_Npm" 
-        style={{ maxWidth: 40, maxHeight: 40,}}
+        style={{ maxWidth: 50, maxHeight: 40,}}
         />
     </div>
   );
