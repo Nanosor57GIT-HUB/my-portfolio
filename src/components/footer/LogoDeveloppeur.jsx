30a5aededbcaeeebc3d13b6675f86a9ec78126ea
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-04-27 19:43:47 +0200

    resize display

--- src/components/footer/LogoDeveloppeur.jsx
+++ src/components/footer/LogoDeveloppeur.jsx
@@ -7,43 +7,43 @@ const LogoDeveloppeur = () => {
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoHTML5.webp"}
         className="logoDev"
         alt="logo_Html5"
-        style={{ minWidth: 25, minHeight: 25,}}
+        style={{ maxWidth: 40, maxHeight: 40,}}
       />
       <img 
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoCSS3.webp"} 
         className="logoDev" 
         alt="logo_Css3" 
-        style={{ minWidth: 25, minHeight: 25,}}
+        style={{ maxWidth: 40, maxHeight: 40,}}
         />
       <img 
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoSass.webp"} 
         className="logoDev" 
         alt="logo_Sass" 
-        style={{ minWidth: 25, minHeight: 25,}}
+        style={{ maxWidth: 40, maxHeight: 40,}}
         />
       <img
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoJavascript.webp"}
         className="logoDev"
         alt="logo_Javascript"
-        style={{ minWidth: 25, minHeight: 25,}}
+        style={{ maxWidth: 40, maxHeight: 40,}}
       />
       <img
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoReact.webp"}
         className="logoDev"
         alt="logo_React"
-        style={{ minWidth: 25, minHeight: 25,}}
+        style={{ maxWidth: 40, maxHeight: 40,}}
       />
       <img
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNodejs.webp"}
         className="logoDev"
         alt="logo_NodeJs"
-        style={{ minWidth: 25, minHeight: 25,}}
+        style={{ maxWidth: 40, maxHeight: 40,}}
       />
       <img 
         src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNPM.webp"} 
         className="logoDev" 
         alt="logo_Npm" 
-        style={{ minWidth: 25, minHeight: 25,}}
+        style={{ maxWidth: 40, maxHeight: 40,}}
         />
     </div>
   );
