1d1ae856c402503b85d8ea2102627a8127be0d58
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-04-29 12:18:28 +0200

    ...

--- src/components/footer/LogoDeveloppeur.jsx
+++ src/components/footer/LogoDeveloppeur.jsx
@@ -4,46 +4,46 @@ const LogoDeveloppeur = () => {
   return (
     <div className="containerLogoDev">
       <img
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoHTML5(50x40).png"}
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoHTML5(30x30).png"}
         className="logoDev"
         alt="logo_Html5"
-        style={{ maxWidth: 50, maxHeight: 40,}}
+        style={{ maxWidth: 30, maxHeight: 30}}  
       />
       <img 
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoCSS3(50x40).png"} 
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoCSS3(30x30).png"}
         className="logoDev" 
         alt="logo_Css3" 
-        style={{ maxWidth: 50, maxHeight: 40,}}
+        style={{ maxWidth: 30 , maxHeight: 30}}
         />
       <img 
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoSass(50x40).png"} 
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoSass(30x30).png"} 
         className="logoDev" 
         alt="logo_Sass" 
-        style={{ maxWidth: 50, maxHeight: 40,}}
+        style={{ maxWidth: 30 , maxHeight: 30}}  
         />
       <img
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoJavascript(50x40).png"}
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoJavascript(30x30).png"}
         className="logoDev"
         alt="logo_Javascript"
-        style={{ maxWidth: 50, maxHeight: 40,}}
+        style={{ maxWidth: 30 , maxHeight: 30}}
       />
       <img
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoReact(50x40).png"}
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoReact(30x30).png"}
         className="logoDev"
         alt="logo_React"
-        style={{ maxWidth: 50, maxHeight: 40,}}
+        style={{ maxWidth: 30, maxHeight: 30}}
       />
       <img
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNodejs(50x40).png"}
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNodejs(30x30).png"}
         className="logoDev"
         alt="logo_NodeJs"
-        style={{ maxWidth: 50, maxHeight: 40,}}
+        style={{ maxWidth: 30, maxHeight: 30}} 
       />
       <img 
-        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNPM(50x40).png"} 
+        src={process.env.PUBLIC_URL + "logoDeveloppeur/logoNPM(30x30).png"}
         className="logoDev" 
         alt="logo_Npm" 
-        style={{ maxWidth: 50, maxHeight: 40,}}
+        style={{ maxWidth: 30, maxHeight: 30}}
         />
     </div>
   );
