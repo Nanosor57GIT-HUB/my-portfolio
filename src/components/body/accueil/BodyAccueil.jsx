d7d93e742368a1b2286f17c65e8c38daca4bc6f3
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-05-10 17:59:37 +0200

    jkl

--- src/components/body/accueil/BodyAccueil.jsx
+++ src/components/body/accueil/BodyAccueil.jsx
@@ -1,48 +1,54 @@
-import React, { useEffect, useRef } from "react";
+//import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./accueil.css";
-import { gsap } from "gsap";
+//import { gsap } from "gsap";
+
 
 //https://www.freecodecamp.org/french/news/react-context-pour-debutants/
 
 const BodyAccueil = () => {
 
-  function animateLetters(textRef) {
-    const letters = textRef.current.textContent?.split("") || [];
+  // function animateLetters(textRef) {
+  //   const letters = textRef.current.textContent?.split("") || [];
   
-    if (!textRef.current) {
-      console.error("textRef.current is null or undefined");
-      return;
-    }
+  //   if (!textRef.current) {
+  //     console.error("textRef.current is null or undefined");
+  //     return;
+  //   }
   
-    textRef.current.textContent = "";
+  //   textRef.current.textContent = "";
   
-    letters.forEach((letter, i) => {
-      const span = document.createElement("span");
-      span.textContent = letter;
-      span.style.opacity = 0;
-      textRef.current.appendChild(span);
+  //   letters.forEach((letter, i) => {
+  //     const span = document.createElement("span");
+  //     span.textContent = letter;
+  //     span.style.opacity = 0;
+  //     textRef.current.appendChild(span);
   
-      gsap.to(span, {
-        opacity: 1,
-        duration: 0.2,
-        delay: i * 0.04,
-      });
-    });
-  }
+  //     gsap.to(span, {
+  //       opacity: 1,
+  //       duration: 0.2,
+  //       delay: i * 0.04,
+  //     });
+  //   });
+  // }
   
-  const textRef = useRef(null);
+  // const textRef = useRef(null);
   
-  useEffect(() => {
-    animateLetters(textRef);
-  }, []);
+  // useEffect(() => {
+  //   animateLetters(textRef);
+  // }, []);
   
 
   return (
     <section id="/" className="bodyAccueil">
       <div className="containerAccueil">
         <div className="containerTitlePortfolio">
-          <h1 className="titlePortfolio notranslate" ref={textRef}>
-            Portfolio Professionnel
+          <h1 className="titlePortfolio notranslate" >  
+  
+       
+      Portfolio Professionnel
+ 
+      
           </h1>
           <span className="responsiveText">( For PC & Tablet )</span>
         </div>
