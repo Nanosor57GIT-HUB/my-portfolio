53698add58799b5d7ec6122d371ce6c9c5d04e2f
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-05-21 20:56:19 +0200

    dfg

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="containerLogoPortfolio">
+    <>
       <img
         src={process.env.PUBLIC_URL + "./logoPortfolio(360x180).webp"}
         className="logo"
         alt="logo_My Portfolio inline"
         style={{ maxWidth: 360, maxHeight: 180}}
       ></img>
-    </div>
+    </>
   );
 };
 
