b3077be7e82191f9e9f2388cdda2b4c72cb7a2e3
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-06-04 21:03:26 +0200

    ii

--- src/components/body/projets/PersonnelCards.jsx
+++ src/components/body/projets/PersonnelCards.jsx
@@ -32,7 +32,7 @@ const PersonnelCards = ({perso}) => {
       </div>
       <div className="containerCards">
         {perso.map((item) => (
-           <div className="cards" key={item.id} data-aos="zoom-in">
+           <div className="cards" key={item.id} data-aos="fade-up">
             <p className="description">{item.description}</p>
           <div className="card" >
             <div className="containerCover" >
