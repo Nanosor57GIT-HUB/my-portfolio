3b0f60419bbf7527fbb954ccaf6528fb7d87142d
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-06-04 18:35:52 +0200

    qq

--- src/components/body/projets/FormationCards.jsx
+++ src/components/body/projets/FormationCards.jsx
@@ -41,7 +41,7 @@ const FormationCards = ({pro}) => {
       </div>
       <div className="containerCards">
         {pro.map((item) => (
-          <div className="cards" key={item.id} >
+          <div className="cards" key={item.id} data-aos="zoom-in">
             <p className="description">{item.description}</p>
             <div className="card" >            
               <div className="containerCover" >
