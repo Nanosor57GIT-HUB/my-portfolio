b8a6ab6fd96b82db39ced69b4ba7f5875ab50640
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2023-06-13 18:14:32 +0200

    cc

--- src/components/body/accueil/BodyAccueil.jsx
+++ src/components/body/accueil/BodyAccueil.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 //import "./accueil.css";
 
-// https://pagespeed.web.dev/analysis/https-my-portfolio-in-line-vercel-app/00y2oheqjh?form_factor=mobile
-// 97% perfs mobile et 100% perfs desktop.
+// https://pagespeed.web.dev/analysis/https-my-portfolio-in-line-vercel-app/6ty5apmkzc?form_factor=mobile
+// 98% perfs mobile et 99% perfs desktop.
 
 const BodyAccueil = () => {
   return (
